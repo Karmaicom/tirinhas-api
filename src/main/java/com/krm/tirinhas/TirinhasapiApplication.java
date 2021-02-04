128adb211b7eece83968a847da719c91054fd153
Karmaicom <karmaicom@gmail.com>
2021-02-04 18:32:25 -0300

    Criação do projeto

---
+++ src/main/java/com/krm/tirinhas/TirinhasapiApplication.java
@@ -0,0 +1,13 @@
+package com.krm.tirinhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TirinhasapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TirinhasapiApplication.class, args);
+	}
+
+}
